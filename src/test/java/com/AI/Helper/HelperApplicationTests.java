4c5125f55e51c90a57194a25e0d968490bf98913
AlexandreModesto <xandy.modest14@gmail.com>
2024-10-11 08:15:32 -0300

    first commit

---
+++ src/test/java/com/AI/Helper/HelperApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.AI.Helper;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HelperApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
